e34be0ad68948c29d44efb881de4697a71751b32
Dragunnai27 <shiroyume005@gmail.com>
2018-10-12 20:25:04 +0700

    add edit,update,show and index listing

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,10 +1,14 @@
 class ArticlesController < ApplicationController
     def index
-
+        @list_articles = Articles.all
     end
     # tạo một articles mới
     def new
         @article = Articles.new
+    end
+
+    def edit
+        @article = Articles.find(params[:id])
     end
 
     # :articles là tên của model
@@ -12,12 +16,28 @@ class ArticlesController < ApplicationController
         #render plain: params[:articles].inspect
         @article = Articles.new(article_params)
         if @article.save
+            #Hiện thông báo
             flash[:notice] = "Article was successfully created"
             redirect_to article_path(@article)
         else
+            #reload lại trang 
             render 'new'
         end
     end
+
+    # Tìm và cập nhật bài viết
+    def update
+        @article = Articles.find(params[:id])
+        if @article.update(article_params)
+            #Hiện thông báo
+            flash[:notice] = "Article was successfully updated"
+            redirect_to article_path(@article)
+        else
+            #reload lại trang 
+            render 'edit'
+        end
+    end
+    
 
     # Tìm theo id để hiển thị    
     def show
